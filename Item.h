f7781ff79426d7230a403b2eb75c4fb03fd08593
MatteoPostiferi <matteo.postiferi@stud.unifi.it>
2023-11-24 12:28:26 +0100

    Creazione classe item

---
+++ Item.h
@@ -0,0 +1,60 @@
+//
+// Created by matte on 24/11/2023.
+//
+
+#ifndef LISTASPESA_ITEM_H
+#define LISTASPESA_ITEM_H
+
+
+#include <string>
+
+class Item {
+public:
+    Item(const std::string &description, const std::string &category, int quantity, bool bought) : description(
+            description), category(category), quantity(quantity), bought(bought) {}
+
+    virtual ~Item() {}
+
+    const std::string &getDescription() const {
+        return description;
+    }
+
+    void setDescription(const std::string &description) {
+        Item::description = description;
+    }
+
+    const std::string &getCategory() const {
+        return category;
+    }
+
+    void setCategory(const std::string &category) {
+        Item::category = category;
+    }
+
+    int getQuantity() const {
+        return quantity;
+    }
+
+    void setQuantity(int quantity) {
+        Item::quantity = quantity;
+    }
+
+    bool isBought() const {
+        return bought;
+    }
+
+    void setBought(bool bought) {
+        Item::bought = bought;
+    }
+
+
+private:
+    std::string description;
+std::string category;
+int quantity;
+bool bought;
+
+};
+
+
+#endif //LISTASPESA_ITEM_H
