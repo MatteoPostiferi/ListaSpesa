aec0b1c630b8d8d82c2b91f017e33b5d7e70c8af
MatteoPostiferi <matteo.postiferi@stud.unifi.it>
2024-02-28 10:14:04 +0100

    RunAllTest

--- Test/RunAllTests.cpp
+++ Test/RunAllTests.cpp
@@ -1,3 +1,11 @@
 //
 // Created by matte on 23/02/2024.
 //
+#include "gtest/gtest.h"
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
